aadac9132248a0c5db4d43d85e2343613db2be7e
Vitreus <Vitreus101@protonmail.com>
2021-11-09 12:55:46 +0100

    wrote `amino_acid`

--- fastmd/src/main.cpp
+++ fastmd/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "files/map_file.h"
+#include "types/amino_acid.h"
 
 int main() {
     return EXIT_SUCCESS;
--- fastmd/src/types/amino_acid.h
+++ fastmd/src/types/amino_acid.h
@@ -3,21 +3,35 @@
 #include "config.h"
 
 namespace fastmd {
-    enum class amino_acid_code: char {
+    enum amino_acid_code: char {
         ALA, ARG, ASN, ASP, CYS, GLU, GLN, GLY, HIS, ILE,
         LEU, LYS, MET, PHE, PRO, SER, THR, TRP, TYR, VAL
     };
 
     class amino_acid {
     public:
-        amino_acid(amino_acid_code code);
-        amino_acid(char letter);
-        amino_acid(std::string const& name);
+        inline explicit amino_acid(amino_acid_code code);
+        inline explicit amino_acid(char letter);
+        inline explicit amino_acid(std::string const& name);
 
-        amino_acid_code get_code() const;
-        char get_letter() const;
-        std::string const& get_name() const;
+        inline operator amino_acid_code() const;
+        inline char get_letter() const;
+        inline std::string const& get_name() const;
 
         static constexpr int TOTAL = 20;
+
+    private:
+        amino_acid_code code;
     };
 }
+
+namespace std {
+    template<>
+    struct hash<fastmd::amino_acid> {
+        size_t operator()(fastmd::amino_acid const& aa) const {
+            return hash<fastmd::amino_acid_code>()(aa.get_code());
+        }
+    };
+}
+
+#include "amino_acid.inl"
